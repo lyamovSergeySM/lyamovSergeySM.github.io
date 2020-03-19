2b7de882a011b7171160abeeee26ce3dd5bbe8ae
LyamovSergey <lyamofff@gmail.com>
2020-03-19 11:40:46 +0200

    регистрация service Worker

---
+++ app/sw.js
@@ -0,0 +1,12 @@
+
+// 'use strict';
+
+// importScripts('sw-toolbox.js');
+
+// toolbox.precache(["index.html","dist/style.main.css"]);
+
+// toolbox.router.get('/images/*', toolbox.cacheFirst);
+
+// toolbox.router.get('/*', toolbox.networkFirst, {
+//   networkTimeoutSeconds: 5
+// });
